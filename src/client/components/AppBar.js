4faad71a012554eed5124f0a46463bb2a9a0642c
rome-777 <rys495@gmail.com>
2022-05-22 20:19:02 -0500

    options prompts fully hooked up and styled

--- src/client/components/AppBar.js
+++ src/client/components/AppBar.js
@@ -3,7 +3,6 @@ import { Container, Box, Typography } from '@mui/material';
 import React, { Fragment } from 'react';
 
 export default function AppBar() {
-
     return (
         <Fragment>
             <Container>
--- src/client/utilities/index.js
+++ src/client/utilities/index.js
@@ -3,5 +3,3 @@ export { messageList } from './message-list';
 export { messageLoading } from './message-loading';
 export { initChatScript, initalOptions, optionResponse } from './prompts';
 export { optionsList } from './options-list';
-
-// export { default as useTimeout } from './X_useTimeout';
